901b3671a7d170bc9034850fe6784f2ac29eb62b
mrndolo <callistomutunga@gmail.com>
2024-06-29 11:59:58 +0300

    Added form behavior functionality

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzbVVTvw5-C01UHTT07GcMnZ_dNWCMbbV3bxd65h_wnP1pf56z6zUpliFdjzMMSKl44/exec'
 const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    .then(response => {msg.innerHTML = "Thank you for subscribing!"
+        setTimeout(function(){msg.innerHTML = ""},3000)
+        form.reset()
+    }
+    )
     .catch(error => console.error('Error!', error.message))
 })
